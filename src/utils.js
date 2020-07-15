1428c8ab2145194f4d4d3248b04f2ac0801557f3
loopbackseal <young961027@gmail.com>
2020-07-15 10:57:49 +0900

    Send mail sucess

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,32 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import mgTransport from "nodemailer-mailgun-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
 
-export const sendMail = (email) => null;
+export const sendMail = (email) => {
+  const options = {
+    auth: {
+      api_key: process.env.MAILGUN_API,
+      domain: process.env.MAILGUN_DOMAIN,
+    },
+  };
+  const client = nodemailer.createTransport(mgTransport(options));
+  return client.sendMail(email);
+};
 
 export const sendSecretMail = (adress, secret) => {
   const email = {
-    from: "nico@prismagram.com",
+    from: "young@loopbackstudio.com",
     to: adress,
     subject: "Login Secret for Prismagram 🔒",
     html: `Hello! Your login secret is ${secret}.<br/> Copy paste on the app/website to login`,
   };
+  return sendMail(email);
 };
